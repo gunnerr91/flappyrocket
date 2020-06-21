c4ec351c9ab01ff8209d9d1d1eef3c174d7d16ea
gunnerr91 <blissvortex@hotmail.com>
2020-06-21 09:50:34 +0100

    checkpoitn lecture 3.48

---
+++ Assets/FlappyRocket.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlappyRocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(0, 4, 0);
+            print("flap away!!");
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}

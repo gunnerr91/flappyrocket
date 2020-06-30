3656c954684f043a2f2bbfdfeeb6336b666482d7
gunnerr91 <blissvortex@hotmail.com>
2020-06-30 08:29:40 +0100

    added oscillator script to obstacles

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector;
+
+    [Range(0,1)]
+    [SerializeField]
+    float movementFactor;
+
+    Vector3 startingPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var offset = movementFactor * movementVector;
+        transform.position = startingPosition + offset;
+    }
+}
